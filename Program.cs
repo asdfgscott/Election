808e8b9dbce657613a3df8627d53c170909f7e88
Scott Truong <asdfgscott@gmail.com>
2021-09-16 18:09:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Election
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
