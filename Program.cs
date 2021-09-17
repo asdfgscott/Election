4b0c3b952354ad3c9793e658e6970e4207f56c38
Scott Truong <asdfgscott@gmail.com>
2021-09-17 01:50:35 -0400

    Got the inputs in, need to make tally system

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Election
 {
@@ -10,13 +11,32 @@ namespace Election
         {
             // string numCan = Console.ReadLine();
 
-            var cans = new Dictionary<string, string>(){
-                {"Marilyn Manson", "Rhinoceros"},
-                {"Jane Doe", "Family Coalition"},
-                {"John Smith", "independent"}
-            };
+            // var cans = new Dictionary<string, string>(){
+            //     {"Marilyn Manson", "Rhinoceros"},
+            //     {"Jane Doe", "Family Coalition"},
+            //     {"John Smith", "independent"}
+            // };
 
-            Console.WriteLine(cans["Marilyn Manson"]);
+            var cans = new Dictionary<string, string>();
+            var votes = new Dictionary<Int32, string>();
+
+            int x = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < x; i++){
+                cans.Add(Console.ReadLine(),Console.ReadLine());
+            }
+            
+
+
+            int voteNum = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < voteNum; i++){
+                votes.Add(i,Console.ReadLine());
+                
+                Console.WriteLine(votes[i]);
+            }
+
+
         }
     }
 }
