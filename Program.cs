ed07c9afd328b09ef9fbf0d5880a59af959ce2e3
Scott Truong <asdfgscott@gmail.com>
2021-09-19 17:06:45 -0400

    Cleaned code

--- Program.cs
+++ Program.cs
@@ -9,31 +9,16 @@ namespace Election
     {
         static void Main(string[] args)
         {
-            // string numCan = Console.ReadLine();
-
-            // var cans = new Dictionary<string, string>(){
-            //     {"Marilyn Manson", "Rhinoceros"},
-            //     {"Jane Doe", "Family Coalition"},
-            //     {"John Smith", "independent"}
-            // };
-        
             var cans = new Dictionary<string, string>();
             var votes = new Dictionary<string, Int32>();
-
             int x = Int32.Parse(Console.ReadLine());
-
             for(int i = 0; i < x; i++){
                 cans.Add(Console.ReadLine(),Console.ReadLine());
             }
-            
             Console.WriteLine("We have " + cans.Count + " cans");
-
             int voteNum = Int32.Parse(Console.ReadLine());
-
             for(int i = 0; i < voteNum; i++){
-
                 string vote = Console.ReadLine();
-
                 if(votes.ContainsKey(vote)){
                     votes[vote] += 1;
                     Console.WriteLine("In here");
@@ -42,12 +27,10 @@ namespace Election
                     Console.WriteLine("Not here");
                     votes.Add(vote,1);
 
-                }
-                
+                } 
             }
             Console.WriteLine("Scott has " + votes["Scott"] + " votes");
             Console.WriteLine("Kim has " + votes["Kim"] + " votes");
-
             var maxVal = votes.Values.Max();
             Console.WriteLine(maxVal);
         }
